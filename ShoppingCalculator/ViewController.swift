c1888aa1aa520949db214b2a1c0e6e382f3f62f3
Chengyue Gong <631019103@qq.com>
2019-01-02 19:53:28 -0500

    Initial Commit

---
+++ ShoppingCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShoppingCalculator
+//
+//  Created by Chengyue Gong on 2019/1/2.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
